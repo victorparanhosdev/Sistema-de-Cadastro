08955f2d7e10a44541f0fe5108c40831403c5337
Victor Paranhos <victor_paranhos@hotmail.com>
2022-11-22 16:38:17 -0300

    Update22-11-22

--- js.js
+++ js.js
@@ -1,57 +1,45 @@
 let botaoMenu = document.querySelector('.caixa-principal-de-cadastro')
-const caixaDeNome = document.querySelector('#input-name')
-const caixaDoValor = document.querySelector('#input-nasc')
 
-const botaoAdicionar = document.querySelector('#btn-adicionar')
-const botaoReset = document.querySelector('#btn-reset')
-const listaDeIntem = document.querySelector('.listando-intem')
+let caixaDeNome = document.querySelector('#input-name')
+let dataDeNascimento = document.querySelector('#input-nasc')
+
+let listaDeIntem = document.querySelector('.listando-intem')
+
+
 
 let produtos = []
 
-botaoReset.addEventListener('click', ()=> {
+
+
+function resetandoTodosElementos() {
     caixaDoValor.value = ''
     caixaDeNome.value = ''
     listaDeIntem.innerHTML = ''
-})
+}
 
-botaoAdicionar.addEventListener('click', ()=> {
+function adicionandoElementoEmLista() {
     let item1 = caixaDeNome.value
-    let item2 = Number(caixaDoValor.value)
+    let item2 = dataDeNascimento.value
     produtos.push(item1, item2)
     let listaAdd = document.createElement('li')
     listaAdd.classList.add('option-lista')
     listaAdd.innerHTML = `
     <span class="span1">NOME:</span> ${item1} <span class="span1">IDADE:</span> <span class="span2">${item2} Anos</span>`
-
+    
     listaDeIntem.appendChild(listaAdd)
     caixaDoValor.value = ''
     caixaDeNome.value = ''
-
-
-})
-
+}
 
 
 
 const Menu = {
     abrir() {
         botaoMenu.classList.add('active')
-    
     },
-
     fechar() {
         botaoMenu.classList.remove('active')
     },
-
-    concluir() {
-
-        botaoMenu.classList.remove('active')
-        
-  
-
-    }
-  
-
 }
 
 
