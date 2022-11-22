5013195eedcb0e464948ad5145c77160d7c3cb23
Victor Paranhos <victor_paranhos@hotmail.com>
2022-11-21 22:24:12 -0200

    Update

--- js.js
+++ js.js
@@ -2,7 +2,36 @@ let botaoMenu = document.querySelector('.menu')
 const caixaDoProduto = document.querySelector('#caixa-peca')
 const caixaDoValor = document.querySelector('#caixa-valor-peca')
 
+const botaoAdicionar = document.querySelector('#btn-adicionar')
+const botaoReset = document.querySelector('#btn-reset')
+const listaDeIntem = document.querySelector('#listando-intem')
+
 let produtos = []
+
+botaoReset.addEventListener('click', ()=> {
+    caixaDoValor.value = ''
+    caixaDoProduto.value = ''
+    listaDeIntem.innerHTML = ''
+})
+
+botaoAdicionar.addEventListener('click', ()=> {
+    let item1 = caixaDoProduto.value
+    let item2 = Number(caixaDoValor.value)
+    produtos.push(item1, item2)
+    let listaAdd = document.createElement('li')
+    listaAdd.classList.add('option-lista')
+    listaAdd.innerHTML = `
+    <span class="span1">NOME:</span> ${item1} <span class="span1">IDADE:</span> <span class="span2">${item2} Anos</span>`
+
+    listaDeIntem.appendChild(listaAdd)
+    caixaDoValor.value = ''
+    caixaDoProduto.value = ''
+
+
+})
+
+
+
 
 const Menu = {
     abrir() {
@@ -16,9 +45,6 @@ const Menu = {
 
     concluir() {
 
-        let item1 = caixaDoProduto.value
-        let item2 = Number(caixaDoValor.value)
-        produtos.push(item1, item2)
         botaoMenu.classList.remove('active')
         
   
@@ -27,6 +53,8 @@ const Menu = {
   
 
 }
+
+
 
 
 
